a8555d580852061f1ef742427dee4d8023c03bdd
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-06-18 15:29:37 +0100

    feature(Fostering Case Update): added About Yourself Update model

---
+++ Models/Fostering/Update/FosteringCaseAboutYourselfUpdateModel.cs
@@ -0,0 +1,26 @@
+﻿namespace StockportGovUK.NetStandard.Models.Models.Fostering.Update
+{
+    public class FosteringCaseAboutYourselfUpdateModel
+    {
+        public FosteringCaseAboutYourselfApplicantUpdateModel FirstApplicant { get; set; }
+
+        public FosteringCaseAboutYourselfApplicantUpdateModel SecondApplicant { get; set; }
+    }
+
+    public class FosteringCaseAboutYourselfApplicantUpdateModel
+    {
+        public bool EverBeenKnownByAnotherName { get; set; }
+
+        public string Nationality { get; set; }
+
+        public string Ethnicity { get; set; }
+
+        public string Gender { get; set; }
+
+        public string AnotherName { get; set; }
+
+        public string SexualOrientation { get; set; }
+
+        public string Religion { get; set; }
+    }
+}

2c2c30e1cb209cbadfb2e0bfea82f718506ee63e
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-19 11:58:55 +0100

    Stephen/Simon added IntegrationFormFieldsUpdateModel

---
+++ Models/Verint/Update/IntegrationFormFieldsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockportGovUK.NetStandard.Models.Models.Verint.Update
+{
+    public class IntegrationFormFieldsUpdateModel
+    {
+        public string CaseReference { get; set; }
+
+        public string IntegrationFormName { get; set; }
+
+        public List<IntegrationFormField> IntegrationFormFields { get; set; }
+    }
+
+    public class IntegrationFormField
+    {
+        public string FormFieldName { get; set; }
+
+        public string FormFieldValue { get; set; }
+    }
+}

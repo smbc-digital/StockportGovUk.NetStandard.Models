0d03c3703b2fd721004c60d83c85dc283129355b
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-20 15:58:08 +0100

    feat(YourEmploymentDetailsUpdateModel)

---
+++ Models/Fostering/Update/FosteringCaseYourEmploymentDetailsUpdateModel.cs
@@ -0,0 +1,24 @@
+﻿using StockportGovUK.NetStandard.Models.Enums;
+
+namespace StockportGovUK.NetStandard.Models.Models.Fostering.Update
+{
+    public class FosteringCaseYourEmploymentDetailsUpdateModel
+    {
+        public string CaseReference { get; set; }
+
+        public FosteringCaseYourEmploymentDetailsApplicantUpdateModel FirstApplicant { get; set; }
+
+        public FosteringCaseYourEmploymentDetailsApplicantUpdateModel SecondApplicant { get; set; }
+    }
+
+    public class FosteringCaseYourEmploymentDetailsApplicantUpdateModel
+    {
+        public bool? AreYouEmployed { get; set; }
+
+        public string CurrentEmployer { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public EHoursOfWork CurrentHoursOfWork { get; set; }
+    }
+}

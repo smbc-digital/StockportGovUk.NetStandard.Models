f1def35af83208dee3d52728f358520d36d486c7
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-20 08:40:22 +0100

    feat(VerintUpdate): Added Model Validation

--- Models/Verint/Update/IntegrationFormFieldsModel.cs
+++ Models/Verint/Update/IntegrationFormFieldsModel.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StockportGovUK.NetStandard.Models.Models.Verint.Update
 {
     public class IntegrationFormFieldsUpdateModel
     {
+        [Required]
         public string CaseReference { get; set; }
 
+        [Required]
         public string IntegrationFormName { get; set; }
 
+        [Required, MinLength(1, ErrorMessage = "At least one item is required")]
         public List<IntegrationFormField> IntegrationFormFields { get; set; }
     }
 }

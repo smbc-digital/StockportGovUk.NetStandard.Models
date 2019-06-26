62e0c81ec0e094fa7e8b72e2c937eb482ac08cde
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-26 13:56:12 +0100

    feat(FosteringUpdate): Added model for your fostering history

---
+++ Models/FosteringCaseYourFosteringHistoryUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿namespace StockportGovUK.NetStandard.Models.Models
+{
+    public class FosteringCaseYourFosteringHistoryUpdateModel
+    {
+        public string CaseReference { get; set; }
+
+        public FosteringCaseYourFosteringHistoryApplicationUpdateModel FirstApplicant { get; set; }
+
+        public FosteringCaseYourFosteringHistoryApplicationUpdateModel SecondApplicant { get; set; }
+    }
+
+    public class FosteringCaseYourFosteringHistoryApplicationUpdateModel
+    {
+        public bool? PreviouslyApplied { get; set; }
+    }
+}

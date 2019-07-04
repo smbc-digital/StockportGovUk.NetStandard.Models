41e2db1c13ec68f37e86bc36ef5c0da945e8c1a1
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-07-04 17:25:33 +0100

    feature(FosteringCase): added FosteringCaseHouseholdUpdateModel

---
+++ Models/Fostering/Update/FosteringCaseHouseholdUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Models.Models.Fostering.Update
+{
+    public class FosteringCaseHouseholdUpdateModel
+    {
+        public string CaseReference { get; set; }
+
+        public bool? AnyOtherPeopleInYourHousehold { get; set; }
+
+        public bool? DoYouHaveAnyPets { get; set; }
+
+        public List<OtherPerson> OtherPeopleInYourHousehold { get; set; }
+    }
+}

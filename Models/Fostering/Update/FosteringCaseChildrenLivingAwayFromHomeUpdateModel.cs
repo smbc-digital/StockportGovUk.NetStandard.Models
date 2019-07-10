b312fe149a411aa1c1a9b6ffec1d571bee0936b1
Jordan <jordan.burns@stockport.gov.uk>
2019-07-10 11:48:21 +0100

    feat(fostering) Merged Under16/Over16 into one update model

---
+++ Models/Fostering/Update/FosteringCaseChildrenLivingAwayFromHomeUpdateModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Models.Models.Fostering.Update
+{
+    public class FosteringCaseChildrenLivingAwayFromHomeUpdateModel
+    {
+        public string CaseReference { get; set; }
+
+        public FosteringCaseChildrenLivingAwayFromHomeApplicantUpdateModel FirstApplicant { get; set; }
+
+        public FosteringCaseChildrenLivingAwayFromHomeApplicantUpdateModel SecondApplicant { get; set; }
+
+    }
+
+    public class FosteringCaseChildrenLivingAwayFromHomeApplicantUpdateModel
+    {
+        public bool? AnyChildrenUnderSixteen { get; set; }
+
+        public List<OtherPerson> ChildrenUnderSixteenLivingAwayFromHome { get; set; }
+
+        public bool? AnyChildrenOverSixteen { get; set; }
+
+        public List<OtherPerson> ChildrenOverSixteenLivingAwayFromHome { get; set; }
+    }
+}

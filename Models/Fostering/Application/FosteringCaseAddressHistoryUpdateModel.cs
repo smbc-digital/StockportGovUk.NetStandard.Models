f21ba7b0d1a01daf69697fbeb0a5be850f994d63
stephen.lally <stephen.lally.smbc@gmail.com>
2019-08-15 15:58:48 +0100

    fix(Address History): fix update model to include first and second applicant

--- Models/Fostering/Application/FosteringCaseAddressHistoryUpdateModel.cs
+++ Models/Fostering/Application/FosteringCaseAddressHistoryUpdateModel.cs
@@ -6,6 +6,13 @@ namespace StockportGovUK.NetStandard.Models.Models.Fostering.Application
     {
         public string CaseReference { get; set; }
 
+        public FosteringCaseAddressHistoryApplicantUpdateModel FirstApplicant { get; set; }
+
+        public FosteringCaseAddressHistoryApplicantUpdateModel SecondApplicant { get; set; }
+    }
+
+    public class FosteringCaseAddressHistoryApplicantUpdateModel
+    {
         public List<PreviousAddress> AddressHistory { get; set; }
     }
 }

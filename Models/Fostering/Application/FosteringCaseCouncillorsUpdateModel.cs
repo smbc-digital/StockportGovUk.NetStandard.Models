31871a3ba0709dee28d98820293ffa404347d219
Jordan <jordan.burns@stockport.gov.uk>
2019-08-19 16:35:42 +0100

    fix(fosteringCaseCouncillorsUpdateModel): remove required attributes

--- Models/Fostering/Application/FosteringCaseCouncillorsUpdateModel.cs
+++ Models/Fostering/Application/FosteringCaseCouncillorsUpdateModel.cs
@@ -20,17 +20,14 @@ namespace StockportGovUK.NetStandard.Models.Models.Fostering.Application
         [Required]
         public bool HasContactWithCouncillor { get; set; }
 
-        [RequiredIf("HasContactWithCouncillor", "The CouncillorRelationshipDetails field is required.")]
         public List<CouncillorRelationshipDetailsUpdateModel> CouncillorRelationshipDetails { get; set; }
     }
 
 
     public class CouncillorRelationshipDetailsUpdateModel
     {
-        [Required]
         public string CouncillorName { get; set; }
 
-        [Required]
         public string Relationship { get; set; }
     }
 }

3eb8eeb20415b011633cc6dd1d32d57e13b00ef6
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-09-09 11:27:22 +0100

    feature(ComplaintDetails): added ComplaintDetails model

--- Models/ComplimentsComplaints/ComplaintDetails.cs
+++ Models/ComplimentsComplaints/ComplaintDetails.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using StockportGovUK.NetStandard.Models.Models.Verint;
 
 namespace StockportGovUK.NetStandard.Models.Models.ComplimentsComplaints
 {
-    class Complaint
+    public class ComplaintDetails
     {
+        public string CouncilDepartment { get; set; }
+
+        public string CouncilDepartmentOther { get; set; }
+
+        public string CouncilDepartmentSub { get; set; }
+
+        public string ComplaintType { get; set; }
+
+        public string StaffName { get; set; }
+
+        public string Complaint { get; set; }
+
+        public string EventCode { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public Address Address { get; set; }
+
     }
 }

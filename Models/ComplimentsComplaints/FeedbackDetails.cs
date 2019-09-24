43bb5626da5905e8b6fba5721db629205d939206
Jon Chiles <jonchiles@yahoo.com>
2019-09-24 14:00:05 +0100

    Feat(Feedback): added feedbackdetails model for comments compliments and feedback

---
+++ Models/ComplimentsComplaints/FeedbackDetails.cs
@@ -0,0 +1,19 @@
+﻿namespace StockportGovUK.NetStandard.Models.Models.ComplimentsComplaints
+{
+    public class FeedbackDetails
+    {
+        public string CouncilDepartment { get; set; }
+
+        public string CouncilDepartmentOther { get; set; }
+
+        public string CouncilDepartmentSub { get; set; }
+
+        public string Compliment { get; set; }
+
+        public string EventCode { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}

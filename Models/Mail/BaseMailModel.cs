aedd46defdb2f9366173c044adc8dd3fa6ba2e93
Elena Secara <elena.secara@stockport.gov.uk>
2019-10-09 16:41:56 +0100

    feature(Mail): added details to BaseMailModel and EMailTemplate

--- Models/Mail/BaseMailModel.cs
+++ Models/Mail/BaseMailModel.cs
@@ -1,10 +1,23 @@
-﻿namespace StockportGovUK.NetStandard.Models.Models.Mail
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Models.Models.Mail
 {
     public class BaseMailModel
     {
         public string RecipientAddress { get; set; }
 
         public string Subject { get; set; }
+
+        public string Content { get; set; }
+
+        public List<MailAttachement> AttachmentsList { get; set; }
+
+        public virtual string TemplateName => "BaseTemplate.html";
+    }
+
+    public class MailAttachement
+    {
+        public string Name { get; set; }
 
         public string Content { get; set; }
     }

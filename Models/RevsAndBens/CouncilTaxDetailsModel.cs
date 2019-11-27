42b9e6ef1ecb3484abe48582d16c0cee387771df
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-11-27 13:53:34 +0000

    feat(CouncilTaxDetails)

---
+++ Models/RevsAndBens/CouncilTaxDetailsModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using StockportGovUK.NetStandard.Models.Models.Civica.CouncilTax;
+using StockportGovUK.NetStandard.Models.RevsAndBens;
+
+namespace StockportGovUK.NetStandard.Models.Models.RevsAndBens
+{
+    public class CouncilTaxDetailsModel
+    {
+        public string PaymentMethod { get; set; }
+        public bool IsDirectDebitCustomer { get; set; }
+        public decimal AmountOwing { get; set; }
+        public IEnumerable<YearTotalResponse> YearTotals { get; set; }
+        public string Reference { get; set; }
+        public PaymentSummaryResponse PaymentSummary { get; set; }
+        public bool? IsFinalNotice { get; set; }
+        public bool? IsClosed { get; set; }
+        public string AccountName { get; set; }
+        public string AccountNumber { get; set; }
+        public string LiabilityPeriodStart { get; set; }
+        public string LiabilityPeriodEnd { get; set; }
+        public List<InstallmentModel> UpcomingPayments { get; set; }
+        public List<TransactionModel> PreviousPayments { get; set; }
+        public List<TransactionModel> TransactionHistory { get; set; }
+        public bool HasBenefits { get; set; }
+        public string TaxBand { get; set; }
+        public string Property { get; set; }
+        public string LatestBillId { get; set; }
+        public List<CouncilTaxDocument> Documents { get; set; }
+    }
+
+    public class InstallmentModel
+    {
+        [DataType("Date")]
+        public DateTime Date { get; set; }
+
+        [DataType("Currency")]
+        public decimal Amount { get; set; }
+
+        public bool IsDirectDebit { get; set; }
+
+        public string Description => IsDirectDebit ? "Direct Debit" : "Non Direct Debit";
+    }
+}

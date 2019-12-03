50002c386809bc67a5d3e56308171417dae658f8
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2019-12-03 13:24:29 +0000

    fix() - Capitalisation on api is important so model is updated to reflect this!

--- Models/Civica/Pay/Request/BasketSecurityDetailsRequest.cs
+++ Models/Civica/Pay/Request/BasketSecurityDetailsRequest.cs
@@ -1,7 +1,15 @@
 namespace StockportGovUK.NetStandard.Models.Civica.Pay.Request
 {
-    public class BasketSecurityDetailsRequest : AuthenticatedRequest
+    public class BasketSecurityDetailsRequest
     {
+        public string CallingAppIdentifier { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public string ApiPassword { get; set; }
+
+        public string UserName { get; set; }
+
         public string BasketReference { get; set; }
     }
 }

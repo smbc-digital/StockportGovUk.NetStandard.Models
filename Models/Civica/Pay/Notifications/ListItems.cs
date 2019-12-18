56e387ad6418088f9b2670b74907b28595c27611
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2019-12-18 15:28:40 +0000

    fix(ListItems model) Make lisitem a list

--- Models/Civica/Pay/Notifications/ListItems.cs
+++ Models/Civica/Pay/Notifications/ListItems.cs
@@ -6,8 +6,8 @@ namespace StockportGovUK.NetStandard.Models.Civica.Pay.Notifications
     [XmlRoot(ElementName = "ListItems", Namespace = "http://schemas.datacontract.org/2004/07/epPaymentNotification")]
     public class ListItems
     {
-        [XmlElement(ElementName = "ListItem", Namespace = "http://schemas.datacontract.org/2004/07/epPaymentNotification")]
-        public ListItem ListItem { get; set; }
+      	[XmlRoot(ElementName="ListItems", Namespace="http://schemas.datacontract.org/2004/07/epPaymentNotification")]
+        public List<ListItem> ListItem { get; set; }
     }
 
 }

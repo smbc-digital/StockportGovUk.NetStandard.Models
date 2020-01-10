e5b047a4e49e86f60dc0d788ac76a2d3841b6a31
stephen.lally <stephen.lally.smbc@gmail.com>
2020-01-10 12:36:12 +0000

    feat(Organisation): organisation search results

---
+++ Models/Verint/Organisation/OrganisationSearchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace StockportGovUK.NetStandard.Models.Models.Verint.Organisation
+{
+    public class OrganisationSearchResult
+    {
+        public string Reference { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}

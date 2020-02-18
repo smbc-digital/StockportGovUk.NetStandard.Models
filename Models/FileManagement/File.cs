16a22f327e36b1bec3786989fbb2bfb2fe9ec022
stephen.lally <stephen.lally.smbc@gmail.com>
2020-02-18 14:49:53 +0000

    feat(FileManagement): Added file model

---
+++ Models/FileManagement/File.cs
@@ -0,0 +1,8 @@
+namespace StockportGovUK.NetStandard.Models.Models.FileManagement
+{
+    public class File
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+    }
+}

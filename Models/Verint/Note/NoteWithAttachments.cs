fdb5f01226ef5345940a6cd7c1deabdd88b91337
stephen.lally <stephen.lally.smbc@gmail.com>
2020-02-19 13:26:06 +0000

    feat(Verint): Added model for note with attachment

---
+++ Models/Verint/Note/NoteWithAttachments.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using StockportGovUK.NetStandard.Models.Models.FileManagement;
+
+namespace StockportGovUK.NetStandard.Models.Verint
+{
+    public class NoteWithAttachments
+    {
+        [Required]
+        [RegularExpression("^[0-9]{12}$", ErrorMessage = "CaseRef must be a valid reference number")]
+        public long CaseRef { get; set; }
+        [Required]
+        public List<File> Attachments { get; set; }
+        public string AttachmentsDescription { get; set; } = string.Empty;
+        public int Interaction { get; set; } = 0;
+    }
+}

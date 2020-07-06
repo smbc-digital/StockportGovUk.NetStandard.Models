a38dd10c98b3f30f264fd804089bb4da13cc9c62
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2020-07-06 13:58:35 +0100

    feature(Verint): add verint online form objects

---
+++ Models/Verint/VerintOnlineForm/VerintOnlineFormRequest.cs
@@ -0,0 +1,18 @@
+﻿using StockportGovUK.NetStandard.Models.Verint;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Models.Models.Verint.VerintOnlineForm
+{
+    public class VerintOnlineFormRequest
+    {
+        [Required]
+        public Case VerintCase { get; set; }
+
+        [Required]
+        public string FormName { get; set; }
+
+        [Required]
+        public IDictionary<string, string> FormData { get; set; }
+    }
+}
---
+++ Models/Verint/VerintOnlineForm/VerintOnlineFormResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace StockportGovUK.NetStandard.Models.Models.Verint.VerintOnlineForm
+{
+    public class VerintOnlineFormResponse
+    {
+        public string VerintCaseReference { get; set; }
+        public string VerintOnlineFormReference { get; set; }
+    }
+}

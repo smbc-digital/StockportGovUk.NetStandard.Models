ce61528410fae908b9b909027c31788f6765614c
michael.lau <michael.lau@stockport.gov.uk>
2020-07-09 11:44:09 +0100

    feature: Add abandoned vehicle model for mailing

---
+++ Models/AbandonedVehicle/AbandonedVehicleMailModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StockportGovUK.NetStandard.Models.AbandonedVehicle
+{
+    public class AbandonedVehicleMailModel
+    {
+        public string Reference { get; set; }
+        public StockportGovUK.NetStandard.Models.Addresses.Address StreetInput { get; set; }
+    }
+}

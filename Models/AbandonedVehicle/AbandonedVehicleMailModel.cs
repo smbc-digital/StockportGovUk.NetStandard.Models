8a11ec624deb3a2f27a44b4bb71d1c6c707231ef
michael.lau <michael.lau@stockport.gov.uk>
2020-07-10 15:42:14 +0100

    fix: update property for address for abandoned vehicle

--- Models/AbandonedVehicle/AbandonedVehicleMailModel.cs
+++ Models/AbandonedVehicle/AbandonedVehicleMailModel.cs
@@ -5,7 +5,7 @@ namespace StockportGovUK.NetStandard.Models.AbandonedVehicle
     public class AbandonedVehicleMailModel : BaseMailModel
     {
         public string Reference { get; set; }
-        public StockportGovUK.NetStandard.Models.Addresses.Address StreetInput { get; set; }
+        public string StreetInput { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }

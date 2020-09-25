b7bd4469e8bd6ea2f127871fab52aa13e13cdd1a
mhayesm <michael.hayes@stockport.gov.uk>
2020-09-25 13:46:49 +0100

    feat: add coronaisolation mailmodel

---
+++ Models/CoronavirusIsolationPayment/CoronavirusIsolationPaymentMailModel.cs
@@ -0,0 +1,9 @@
+using StockportGovUK.NetStandard.Models.Mail;
+
+namespace StockportGovUK.NetStandard.Models.Models.AccessProtectionMarkings
+{
+    public class CoronavirusIsolationPaymentMailModel : BaseMailModel
+    {
+        public string Reference { get; set; }
+    }
+}

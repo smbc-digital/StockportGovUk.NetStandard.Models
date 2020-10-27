4b4bfbe6da68c6df9715a34af26948a79a96f901
michaellau-smbc <michael.lau@stockport.gov.uk>
2020-10-27 15:11:45 +0000

    chore: add booking response entity

---
+++ Models/Booking/Response/AvailabilityResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StockportGovUK.NetStandard.Models.Models.Booking.Response
+{
+    public class AvailabilityDayResponse
+    {
+        public DateTime Date { get; set; }
+        public List<AppointmentTime> AppointmentTimes { get; set; }
+        public bool HasAvailableAppointment => AppointmentTimes.Any();
+    }
+
+    public class AppointmentTime
+    {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}

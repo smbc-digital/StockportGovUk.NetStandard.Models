081d16e12aef80fa9c67233f73872cca4105ced7
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-11-18 11:08:47 +0000

    chore(BookingRequest): add request models for bookings

---
+++ Models/Booking/Request/ConfirmationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Models.Models.Booking.Request
+{
+    public class ConfirmationRequest
+    {
+        [Required]
+        public Guid BookingId { get; set; }
+
+        public List<BookingResource> OptionalResources { get; set; }
+    }
+}

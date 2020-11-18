56ee208086b131789062e7e97e582253f9eaf8fe
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-11-18 11:08:47 +0000

    chore(BookingRequest): add request models for bookings

---
+++ Models/Booking/Request/AvailabilityRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Models.Models.Booking.Request
+{
+    public class AvailabilityRequest
+    {
+        [Required]
+        public Guid AppointmentId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public List<BookingResource> OptionalResources { get; set; }
+    }
+}

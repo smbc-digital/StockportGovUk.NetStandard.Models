8dab0c7da496119fcbb173ec77a0ceecf3926142
stephen.lally <stephen.lally.smbc@gmail.com>
2020-11-18 17:12:41 +0000

    chore(booking): fixed namespace for booking request/responses

--- Models/Booking/Response/AvailabilityDayResponse.cs
+++ Models/Booking/Response/AvailabilityDayResponse.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace StockportGovUK.NetStandard.Models.Models.Booking.Response
+namespace StockportGovUK.NetStandard.Models.Booking.Response
 {
     public class AvailabilityDayResponse
     {
         public DateTime Date { get; set; }
         public List<AppointmentTime> AppointmentTimes { get; set; } = new List<AppointmentTime>();
         public bool HasAvailableAppointment => AppointmentTimes.Any();
-        public bool FullDay => AppointmentTimes.Count == 1 &&
+        public bool IsFullDayAppointment => AppointmentTimes.Count == 1 &&
             AppointmentTimes.First().Duration.Hours >=6 &&
             AppointmentTimes.First().StartTime.Hours < 12 && 
-            AppointmentTimes.First().EndTime.Hours > 12;      
+            AppointmentTimes.First().EndTime.Hours > 12;
     }
 
     public class AppointmentTime

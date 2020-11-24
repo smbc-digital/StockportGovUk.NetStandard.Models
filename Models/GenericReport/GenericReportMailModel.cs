2b7f1bf5c567f58d4363b859d1497b0cb698d7a9
Harry <harry.woodall@stockport.gov.uk>
2020-11-24 15:29:17 +0000

    feat(Generic Report): add generic report

---
+++ Models/GenericReport/GenericReportMailModel.cs
@@ -0,0 +1,13 @@
+﻿using StockportGovUK.NetStandard.Models.Mail;
+
+namespace StockportGovUK.NetStandard.Models.Models.StreetReport
+{
+    public class GenericReportMailModel : BaseMailModel
+    {
+        public string Reference { get; set; }
+        public bool ShowRefNumber { get; set; } = false;
+        public string FormText { get; set; }
+        public bool HasNext { get; set; } = false;
+        public string HappensNextContent { get; set; }
+    }
+}

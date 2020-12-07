7b90a57ee02941d93c814c9bfa9898a5be70681d
stephen.lally <stephen.lally.smbc@gmail.com>
2020-12-07 10:37:57 +0000

    feat(booking): booking entity

---
+++ Models/Booking/Booking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StockportGovUK.NetStandard.Models.Booking
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}

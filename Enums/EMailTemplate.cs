76ffad28f8b36590e0e26c5c13fb6d0e030e3b02
Priya Ganger <priya.ganger@stockport.gov.uk>
2020-12-21 16:03:37 +0000

    add AdvisoryDisabledParkingBay model and to the enum

--- Enums/EMailTemplate.cs
+++ Enums/EMailTemplate.cs
@@ -65,6 +65,7 @@
         CoronavirusIsolationPayment,
         StockportLocalFund,
         SportsVenue,
-        GenericReport
+        GenericReport,
+        AdvisoryDisabledParkingBay
     }
 }

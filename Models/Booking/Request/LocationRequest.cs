2531adf2ba7128c466d70e439cc74f0329d33132
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2020-12-23 13:37:50 +0000

    fix(booking): fix namespace (#71)

--- Models/Booking/Request/LocationRequest.cs
+++ Models/Booking/Request/LocationRequest.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using StockportGovUK.NetStandard.Models.Booking.Request;
 
-namespace StockportGovUK.NetStandard.Models.Models.Booking.Request
+namespace StockportGovUK.NetStandard.Models.Booking.Request
 {
     public class LocationRequest
     {

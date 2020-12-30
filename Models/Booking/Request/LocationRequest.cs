3d0bf95664c5885e3b808909bdb88d31425a1cd9
michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
2020-12-30 11:52:59 +0000

    build:release 1.2.0 (#73)
    * chore(booking): fixed namespace for booking models, changed FullDay property (#56)
    
    * chore(booking): fixed namespace for booking request/responses
    
    * chore(project): changed package version
    
    * chore(project): bump patch version
    
    * feat(Generic Report): add generic report
    
    * chore(Generic Report): change namespace
    
    * chore(Booking): Added duation calculation on AppointmentTime (#60)
    
    * chore(Generic Report): update mail model
    
    * chore(booking): updated request to include first/lastname and Address object
    
    * chore(project): bump version
    
    * feat(booking): booking entity
    
    * chore(booking): Added booking endtime to model (#66)
    
    * chore: bump version
    
    * add AdvisoryDisabledParkingBay model and to the enum
    
    * feat(booking): add LocationRequest
    
    * fix(booking): fix namespace (#71)
    
    * chore(version) 1.2.0
    
    Co-authored-by: lallysmbc <35955528+lallysmbc@users.noreply.github.com>
    Co-authored-by: stephen.lally <stephen.lally.smbc@gmail.com>
    Co-authored-by: Harry <harry.woodall@stockport.gov.uk>
    Co-authored-by: michael hayes <michael.hayes@stockport.gov.uk>
    Co-authored-by: Colin Lees <colin.lees@stockport.gov.uk>
    Co-authored-by: Priya Ganger <priya.ganger@stockport.gov.uk>
    Co-authored-by: jordan-burns <32516365+jordan-burns@users.noreply.github.com>
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>
    Co-authored-by: Lydia Gray <34089008+lydiagray@users.noreply.github.com>

---
+++ Models/Booking/Request/LocationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Models.Booking.Request
+{
+    public class LocationRequest
+    {
+        [Required]
+        public Guid AppointmentId { get; set; }
+
+        public List<BookingResource> OptionalResources { get; set; }
+    }
+}

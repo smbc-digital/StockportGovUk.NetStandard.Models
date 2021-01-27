1098970ffcd78e05986dc8bee7d9c3602c0cc3e0
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2021-01-27 13:10:40 +0000

    feat(UpdateAdditionalInformationRequest): add model for UpdateAdditionalInformationRequest (#81)

---
+++ Models/Booking/Request/UpdateAdditionalInformationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StockportGovUK.NetStandard.Models.Booking.Request
+{
+    public class UpdateAdditionalInformationRequest
+    {
+        public Guid BookingId { get; set; }
+        public string AdditionalInformation { get; set; }
+    }
+}

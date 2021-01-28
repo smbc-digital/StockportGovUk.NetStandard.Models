908717688926425aeded36095209563f13a6ea44
mhayesm <michael.hayes@stockport.gov.uk>
2021-01-28 15:06:03 +0000

    chore(mail-model): add header to generic report

--- Models/GenericReport/GenericReportMailModel.cs
+++ Models/GenericReport/GenericReportMailModel.cs
@@ -5,6 +5,7 @@ namespace StockportGovUK.NetStandard.Models.Models.GenericReport
     public class GenericReportMailModel : BaseMailModel
     {
         public string Reference { get; set; }
+        public string Header { get; set; }
         public bool ShowRefNumber { get; set; } = false;
         public string FormText { get; set; }
         public bool HasNext { get; set; } = false;

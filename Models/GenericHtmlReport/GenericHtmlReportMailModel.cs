478bb8f8df7cf530104f95a6c01210d4fa4e504c
mhayesm <michael.hayes@stockport.gov.uk>
2021-02-04 15:44:55 +0000

    feat(genericHtml): add genericHtml model & enum

---
+++ Models/GenericHtmlReport/GenericHtmlReportMailModel.cs
@@ -0,0 +1,12 @@
+﻿using StockportGovUK.NetStandard.Models.Mail;
+
+namespace StockportGovUK.NetStandard.Models.Models.GenericHtmlReport
+{
+    public class GenericHtmlReportMailModel : BaseMailModel
+    {
+        public string Reference { get; set; }
+        public string Header { get; set; }
+        public bool ShowRefNumber { get; set; } = false;
+        public string FormText { get; set; }
+    }
+}

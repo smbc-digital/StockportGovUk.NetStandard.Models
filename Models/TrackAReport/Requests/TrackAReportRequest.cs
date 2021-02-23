9681afd37bdc9b2e2dabb53822dffbd03cee6d42
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-02-23 15:08:00 +0000

    refactor(TrackAReport): add Attributes

--- Models/TrackAReport/Requests/TrackAReportRequest.cs
+++ Models/TrackAReport/Requests/TrackAReportRequest.cs
@@ -1,13 +1,18 @@
-﻿using StockportGovUK.NetStandard.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using StockportGovUK.NetStandard.Models.Enums;
 
 namespace StockportGovUK.NetStandard.Models.TrackAReport.Requests
 {
     public class TrackAReportRequest
     {
+        [Required]
+        [MaxLength(24)]
         public string ReferenceNumber { get; set; }
 
+        [Required]
         public EAssetType AssetType { get; set; }
         
+        [Required]
         public string AssetId { get; set; }
     }
 }

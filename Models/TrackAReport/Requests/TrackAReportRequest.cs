6c00f155cbf961acb3c97f570a9075c2ecf0796b
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2021-02-23 15:35:45 +0000

    feat(TrackAReport): add model and enum class for TrackAReport (#93)
    * feat(TrackAReport): add model and enum class for TrackAReport
    
    * refactor(TrackAReport): add Attributes

---
+++ Models/TrackAReport/Requests/TrackAReportRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using StockportGovUK.NetStandard.Models.Enums;
+
+namespace StockportGovUK.NetStandard.Models.TrackAReport.Requests
+{
+    public class TrackAReportRequest
+    {
+        [Required]
+        [MaxLength(24)]
+        public string ReferenceNumber { get; set; }
+
+        [Required]
+        public EAssetType AssetType { get; set; }
+        
+        [Required]
+        public string AssetId { get; set; }
+    }
+}

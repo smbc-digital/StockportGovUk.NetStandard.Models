7ac7976febdfe8f4a5be342b7d21de8c57874f44
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-02-23 15:03:54 +0000

    feat(TrackAReport): add model and enum class for TrackAReport

---
+++ Models/TrackAReport/Requests/TrackAReportRequest.cs
@@ -0,0 +1,13 @@
+﻿using StockportGovUK.NetStandard.Models.Enums;
+
+namespace StockportGovUK.NetStandard.Models.TrackAReport.Requests
+{
+    public class TrackAReportRequest
+    {
+        public string ReferenceNumber { get; set; }
+
+        public EAssetType AssetType { get; set; }
+        
+        public string AssetId { get; set; }
+    }
+}

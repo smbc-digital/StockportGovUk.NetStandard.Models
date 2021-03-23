b120f6cf5ed8cb3d1815264f9ba742119dec5eee
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2021-03-23 10:25:27 +0000

    minor(Booking): add HashedId to Booking model (#110)

--- Models/Booking/Booking.cs
+++ Models/Booking/Booking.cs
@@ -5,6 +5,7 @@ namespace StockportGovUK.NetStandard.Models.Booking
     public class Booking
     {
         public Guid Id { get; set; }
+        public string HashedId { get; set; }
         public DateTime Date { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }

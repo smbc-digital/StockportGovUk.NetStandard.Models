37e8a4a0ccbf2a6f9753dd4e4cc34375eafbfd98
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2021-04-07 09:37:34 +0100

    minor: add noteswithattachements to case object - change CaseId to be no lnger required

--- Models/Verint/Note/NoteWithAttachments.cs
+++ Models/Verint/Note/NoteWithAttachments.cs
@@ -6,13 +6,16 @@ namespace StockportGovUK.NetStandard.Models.Verint
 {
     public class NoteWithAttachments
     {
-        [Required]
-        [RegularExpression("^[0-9]{12}$", ErrorMessage = "CaseRef must be a valid reference number")]
-        public long CaseRef { get; set; }
+        [RegularExpression("^$|^[0-9]{12}$", ErrorMessage = "CaseRef must be a valid reference number")]
+        public long CaseRef { get; set; } 
+        
         [Required]
         public List<File> Attachments { get; set; }
+
         public string AttachmentsDescription { get; set; } = string.Empty;
+
         public int Interaction { get; set; } = 0;
+
         public string NoteName { get; set; }
     }
 }

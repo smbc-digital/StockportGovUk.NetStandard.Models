0192522839e2332c70dab542d6bf38a879804f73
Simon Estill <simon.estill@stockport.gov.uk>
2021-06-18 15:05:12 +0100

    Created NoiseNuisamce and BonfireNuisance Uniform Models

---
+++ Models/Uniform/BonfireNuisanceServicRequest.cs
@@ -0,0 +1,17 @@
+﻿using StockportGovUK.NetStandard.Models.Verint;
+
+namespace StockportGovUK.NetStandard.Models.Models.Uniform
+{
+    class BonfireNuisanceServiceRequest
+    {
+        public string BonfireNuisanceCode { get; set; }
+        public string Description { get; set; }
+        public string CrmReference { get; set; }
+        public Address Property { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
+
+}
+}
---
+++ Models/Uniform/NoiseNuisanceServiceRequest.cs
@@ -0,0 +1,15 @@
+﻿using StockportGovUK.NetStandard.Models.Verint;
+
+namespace StockportGovUK.NetStandard.Models.Models.Uniform
+{
+    class NoiseNuisanceServiceRequest
+    {
+
+        public string NoiseNuisanceTypeCode { get; set; }
+        public string NoiseNuisanceType { get; set; }
+        public string Description { get; set; }
+        public string CrmReference { get; set; }
+        public Address Property { get; set; }
+        public Customer Customer { get; set; }
+    }
+}

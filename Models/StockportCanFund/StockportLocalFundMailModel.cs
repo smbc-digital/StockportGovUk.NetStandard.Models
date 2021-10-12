a4125f2a02eeeb9971466fe7e39357c70c8f2aba
Jordan <jordan.burns@stockport.gov.uk>
2021-10-12 15:23:16 +0100

    feat(Stockport Can Fund): Add mail entity

---
+++ Models/StockportCanFund/StockportLocalFundMailModel.cs
@@ -0,0 +1,9 @@
+﻿using StockportGovUK.NetStandard.Models.Mail;
+
+namespace StockportGovUK.NetStandard.Models.StockportCanFund
+{
+    public class StockportCanFundMailModel : BaseMailModel
+    {
+        public string Reference { get; set; }
+    }
+}

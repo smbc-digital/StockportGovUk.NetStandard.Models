34f664841e71f9b77cacdd4ab0674c7e4104f59a
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2021-10-21 14:31:45 +0100

    minor: add shared models for formbuilder post payment

---
+++ Models/FormBuilder/EPaymentStatusEnum.cs
@@ -0,0 +1,10 @@
+namespace StockportGovUK.NetStandard.Models.FormBuilder
+{
+    public enum EPaymentStatus
+    {
+        Success = 0,
+        Failure = 1,
+        Cancelled = 2,
+        Declined = 3
+    }    
+}

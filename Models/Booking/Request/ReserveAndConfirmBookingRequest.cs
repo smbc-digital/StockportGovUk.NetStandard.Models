ef483017bb14a42aad31384c860c657b912aa48e
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2022-04-25 15:31:15 +0100

    minor: add new request object for booking and immediate confirmation

---
+++ Models/Booking/Request/ReserveAndConfirmBookingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Models.Booking.Request
+{
+    public class ReserveAndConfirmBookingRequest : BookingRequest
+    {
+        public ReserveAndConfirmBookingRequest() : base() { }
+
+        public ReserveAndConfirmBookingRequest(Customer customer, DateTime startDateTime, Guid appointmentId, DateTime appointmentSearchEndDateTime)
+            : base(customer, startDateTime, appointmentId) => AppointmentSearchEndDateTime = appointmentSearchEndDateTime;
+
+        [Required]
+        public DateTime AppointmentSearchEndDateTime { get; set; } = DateTime.Today.AddMonths(3);
+    }
+}

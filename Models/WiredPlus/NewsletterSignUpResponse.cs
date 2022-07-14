23125c6248cdb769b13235e4f67ef5b9d2bbae10
Stu <h.s.j.wilkes@gmail.com>
2022-07-14 06:41:12 +0100

    feat(WiredPlus): minor add request/response models

---
+++ Models/WiredPlus/NewsletterSignUpResponse.cs
@@ -0,0 +1,8 @@
+namespace StockportGovUK.NetStandard.Gateways.WiredPlus
+{
+    public class NewsletterSignUpResponse
+    {
+        public string id { get; set; }
+        public string message { get; set; }
+    }
+}
